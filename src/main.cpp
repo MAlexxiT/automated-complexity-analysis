ae67694992eec44ad28be631c3fc1df37c6c944a
MAlexxiT <mauricio230600@gmail.com>
2024-05-25 23:22:22 -0600

    Added an extern to yyin and modified control flow in order to allow the resulting binary to take file names as arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 #include "nodos.hpp"
-#include <string> 
-#include <algorithm>
-#include <map>
-#include <set>
 #include "analizadorPrecedencia.hpp"
 extern cl_raiz* raiz;
+extern FILE * yyin;
 extern int yyparse();
 extern void analizador_complejidad(std::vector<cl_declaracion_funcion*> orden_topologico);
 
 int main(int argc, char **argv)
 {
+    //toma el archivo del primer argumento
+    FILE * file = fopen ( argv [1] ,"r");
+    if(file == NULL){
+        cout<<"archivo inexistente"<<endl;
+        return 0;
+    }else{
+        yyin = file ;
+    }
     yyparse();
     analizador_precedencia analizadorPrecedencia(raiz);
-    analizadorPrecedencia.esRecursivo();
+    if(analizadorPrecedencia.esRecursivo()){
+        cout<<"El código fuente contiene por lo menos una dependencia circular de funciones"<<endl;
+        return 0;
+    }
 
     analizador_complejidad(analizadorPrecedencia.orden_topologico);
     return 0;
